1e9178693fed7050bd1b78a5febbd3879626213c
Eroscodex <karlalondra2003@gmail.com>
2026-08-16 19:23:32 +0800

    Add textbee config, SmsService, TextbeeChannel, and SendSmsJob

---
+++ app/Services/SmsService.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class SmsService
+{
+    private string $baseUrl = 'https://api.textbee.dev/api/v1/gateway/send-sms';
+
+    public function send(string|array $recipients, string $message): array
+    {
+        $deviceId = config('textbee.device_id');
+        $apiKey = config('textbee.api_key');
+
+        if (empty($apiKey) || empty($deviceId)) {
+            Log::warning('Textbee SMS skipped: TEXTBEE_API_KEY or TEXTBEE_DEVICE_ID not set.');
+
+            return [
+                'success' => false,
+                'message' => 'Credentials not configured',
+            ];
+        }
+
+        try {
+            $formattedRecipients = array_map(function ($r) {
+                $num = preg_replace('/[^0-9+]/', '', trim($r));
+                if (str_starts_with($num, '09')) {
+                    return '+63'.substr($num, 1);
+                } elseif (str_starts_with($num, '639')) {
+                    return '+'.$num;
+                } elseif (str_starts_with($num, '9') && strlen($num) === 10) {
+                    return '+63'.$num;
+                }
+
+                return $num;
+            }, (array) $recipients);
+
+            $response = Http::withHeader('x-api-key', $apiKey)
+                ->timeout(15)
+                ->asJson()
+                ->post($this->baseUrl, [
+                    'deviceId' => $deviceId,
+                    'recipients' => $formattedRecipients,
+                    'message' => $message,
+                ]);
+
+            $data = $response->json();
+
+            Log::info('Textbee.dev SMS gateway response', [
+                'status' => $response->status(),
+                'response' => $data,
+                'recipients' => $formattedRecipients,
+            ]);
+
+            return $data['data'] ?? $data ?? ['success' => $response->successful()];
+        } catch (\Throwable $e) {
+            Log::error('Textbee.dev SMS Exception: '.$e->getMessage());
+
+            return [
+                'success' => false,
+                'message' => $e->getMessage(),
+            ];
+        }
+    }
+}
---
+++ config/textbee.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'device_id' => env('TEXTBEE_DEVICE_ID'),
+    'api_key' => env('TEXTBEE_API_KEY'),
+];
