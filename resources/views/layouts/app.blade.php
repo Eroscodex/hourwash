7f91271467bc265499f052c4ee7b441f4420cf84
Eroscodex <karlalondra2003@gmail.com>
2026-08-21 12:31:39 +0800

    Update Manage Machines sidebar subtitle to Add, Edit, & Manage Machines

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -143,7 +143,7 @@
                                 <svg class="w-5 h-5 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
                                 <div class="sidebar-text">
                                     <span class="block leading-snug text-[11.5px] font-bold whitespace-nowrap">Manage Machines</span>
-                                    <p class="text-[10px] opacity-75 font-normal block leading-tight mt-0.5 whitespace-nowrap">Machine Status & Fleet Monitor</p>
+                                    <p class="text-[10px] opacity-75 font-normal block leading-tight mt-0.5 whitespace-nowrap">Add, Edit, & Manage Machines</p>
                                 </div>
                             </a>
 
@@ -227,7 +227,7 @@
                                 <svg class="w-5 h-5 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
                                 <div class="sidebar-text">
                                     <span class="block leading-snug">Manage Machines</span>
-                                    <p class="text-[10.5px] opacity-75 font-normal block leading-tight mt-0.5">Machine Status & Fleet Monitor</p>
+                                    <p class="text-[10.5px] opacity-75 font-normal block leading-tight mt-0.5">Add, Edit, & Manage Machines</p>
                                 </div>
                             </a>
 
